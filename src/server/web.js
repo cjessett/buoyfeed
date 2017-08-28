945cfff6243e75199887cb496f16a46f03c20134
CJ Jessett <cjessett@gmail.com>
2017-08-28 00:24:57 -0500

    Organize session middleware

--- src/server/web.js
+++ src/server/web.js
@@ -2,18 +2,11 @@ import express from 'express'
 import shrinkRay from 'shrink-ray'
 import morgan from 'morgan'
 import bodyParser from 'body-parser'
-import session from 'express-session'
-import redis from 'connect-redis'
-import { serveStatic, cacheControl, strictTransportSecurity } from './middleware'
+import { serveStatic, cacheControl, strictTransportSecurity, session } from './middleware'
 import { root, buoys, favorites, auth } from './routes'
 
-const RedisStore = redis(session)
 
 export default (app) => {
-  const store = process.env.NODE_ENV === 'production' ?
-    new RedisStore({ host: process.env.REDIS_HOST, port: process.env.REDIS_PORT }) :
-    undefined
-  const secret = process.env.SESSION_SECRET || 'waves'
   // likely our proxy will handle compression, cache-control, etc. these are healthy defaults
   const web = express()
   web.disable('x-powered-by')
@@ -24,7 +17,7 @@ export default (app) => {
   web.use(bodyParser.json())
   web.use(bodyParser.urlencoded({ extended: true }))
   web.use(morgan('dev'))
-  web.use(session({ secret, resave: false, saveUninitialized: true, store }))
+  web.use(session)
   web.use('/auth', auth(app))
   web.use('/favorites', favorites(app))
   web.use('/buoys', buoys(app))
